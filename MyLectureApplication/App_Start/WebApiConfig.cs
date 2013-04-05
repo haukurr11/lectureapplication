66d9b359d7b85b027dc6ad8c8720a6b86e694fdd
haukurr11 <haukurr11@ru.is>
2013-04-05 18:26:11 +0000

    project

--- MyLectureApplication/App_Start/WebApiConfig.cs
+++ MyLectureApplication/App_Start/WebApiConfig.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using MyLectureApplication.Controllers;
+
 namespace MyLectureApplication
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/v1/lectures/{LectureID}/{controller}/{commentID}",
                 defaults: new { id = RouteParameter.Optional,controller="CommentsController" }
             );
+            config.Routes.MapHttpRoute(
+               name: "Lectures",
+               routeTemplate: "api/v1/{controller}/{LectureID}",
+               defaults: new { id = RouteParameter.Optional, controller = "LecturesController" }
+           );
         }
     }
 }
