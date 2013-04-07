b96b0ae097ec46d35146d362bc0c0c98a66934a9
haukurr11 <haukurr11@ru.is>
2013-04-07 11:19:49 +0000

    fix

--- New folder/webExam/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
-                       url(r'^$', 'webExam.views.index'),
-                       (r'(\d+)', 'webExam.views.webexam'),
-)
